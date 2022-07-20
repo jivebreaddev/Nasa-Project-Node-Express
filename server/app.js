da1bafe9d6c455e3d1d418cb49e029d241f8e0a4
Jivebreaddev <jivebreaddev@gmail.com>
2022-07-20 20:04:49 +0900

    added build and static path

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const planetsRouter = require('./src/routes/planets/planets.router')
 const app = express();
 
@@ -7,6 +8,9 @@ app.use(cors({
     origin: 'http://localhost:3000'
 }));
 app.use(express.json()); // parse any json data
+app.use(express.static(path.join(__dirname,'public')));
 app.use(planetsRouter);
-
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname,'public','index.html'))
+})
 module.exports = app;
