22bc83baf482cd76317355198dd300f0b0319b03
Jivebreaddev <jivebreaddev@gmail.com>
2022-07-20 16:44:04 +0900

    express, http server listener set

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,12 @@
-const express = require('express');
-
+const http = require('http');
+const app = require('./app');
+const server = http.createServer(app);
 const PORT = process.env.PORT || 8000;
+
+server.listen(PORT, () => {
+    console.log('Listening on port ${PORT}...')
+});
+
+
+
 
