66634199cafae62c870eccbec653c87e4de96ef4
Jivebreaddev <jivebreaddev@gmail.com>
2022-07-20 19:29:32 +0900

    npm package added for backend and frontend

--- server/server.js
+++ server/server.js
@@ -2,7 +2,14 @@ const http = require('http');
 const app = require('./app');
 const server = http.createServer(app);
 const PORT = process.env.PORT || 8000; // cross-env for all platforms
+const { loadPlanetsData } = require('./src/models/planets.model');
 
-server.listen(PORT, () => {
-    console.log(`Listening on port ${PORT}...`)
-});
+async function startServer(){
+    await loadPlanetsData();
+    server.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}...`)
+    });
+}
+
+startServer();
+// .....
